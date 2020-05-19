cbd77fc0b2d0c239824b19152ff026728b9f7100
Game <ilja200303@gmail.com>
2020-05-19 13:16:25 +0300

    Add project files.

---
+++ Triangle_Vorm/Triangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Vorm
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public double GetSetA 
+        {
+            get 
+            {return a;}
+            set 
+            {a = value;}
+        }
+
+        public double GetSetB 
+        {
+            get
+            {return b;}
+            set
+            {b = value;}
+        }
+
+        public double GetSetC 
+        {
+            get
+            {return c;}
+            set
+            {c = value;}
+        }
+
+        public bool ExistTriangle 
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+
+    }
+}
