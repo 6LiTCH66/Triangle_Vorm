39dfdc6bf5cd521ca36635770f538d00e3857054
Game <ilja200303@gmail.com>
2020-06-09 14:18:46 +0300

    предупреждение об ошибке если поля пустые, чистит listView1 если там что-то есть

--- Triangle_Vorm/Form1.cs
+++ Triangle_Vorm/Form1.cs
@@ -24,53 +24,54 @@ namespace Triangle_Vorm
 
         private void Run_button_Click(object sender, EventArgs e)
         {
-            double a, b, c;
-            a = Convert.ToDouble(txtA.Text);
-            b = Convert.ToDouble(txtB.Text);
-            c = Convert.ToDouble(txtC.Text);
-
-            
-            Triangle triangle = new Triangle(a, b, c);
-
-          
-            listView1.Items.Add("Сторона а");
-            listView1.Items.Add("Сторона b");
-            listView1.Items.Add("Сторона c");
-
-            listView1.Items.Add("Периметр");
-            listView1.Items.Add("Площадь");
-            listView1.Items.Add("Полупериметр");
-
-            listView1.Items.Add("Существует?");
-            listView1.Items.Add("Спецификатор");
-            
-            listView1.Items[0].SubItems.Add(triangle.outputA());
-            listView1.Items[1].SubItems.Add(triangle.outputB());
-            listView1.Items[2].SubItems.Add(triangle.outputC());
-
-            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
-            
-            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
-
-            listView1.Items[5].SubItems.Add(Convert.ToString(triangle.SemiPerimeter()));
-
-            listView1.Items[7].SubItems.Add(triangle.TypeOfTirangle);
-
-            if (triangle.ExistTriangle) { listView1.Items[6].SubItems.Add("Существует"); }
-            else listView1.Items[6].SubItems.Add("Не существует");
-
-            try
+            if (listView1.Items.Count > 0)
             {
-                if (string.IsNullOrEmpty(txtA.Text) || string.IsNullOrEmpty(txtB.Text) || string.IsNullOrEmpty(txtC.Text))
-                {
-                    MessageBox.Show("Не все поля были заполнены");
-                }
+                listView1.Items.Clear();
             }
-            catch
-            {
-                MessageBox.Show("Все поля были заполнены");
 
+            if (txtA.Text.Length > 0 && txtB.Text.Length > 0 && txtC.Text.Length > 0)
+            {
+
+                double a, b, c;
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                c = Convert.ToDouble(txtC.Text);
+
+
+                Triangle triangle = new Triangle(a, b, c);
+
+
+                listView1.Items.Add("Сторона а");
+                listView1.Items.Add("Сторона b");
+                listView1.Items.Add("Сторона c");
+
+                listView1.Items.Add("Периметр");
+                listView1.Items.Add("Площадь");
+                listView1.Items.Add("Полупериметр");
+
+                listView1.Items.Add("Существует?");
+                listView1.Items.Add("Спецификатор");
+
+                listView1.Items[0].SubItems.Add(triangle.outputA());
+                listView1.Items[1].SubItems.Add(triangle.outputB());
+                listView1.Items[2].SubItems.Add(triangle.outputC());
+
+                listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
+
+                listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
+
+                listView1.Items[5].SubItems.Add(Convert.ToString(triangle.SemiPerimeter()));
+
+                listView1.Items[7].SubItems.Add(triangle.TypeOfTirangle);
+
+                if (triangle.ExistTriangle) { listView1.Items[6].SubItems.Add("Существует"); }
+                else listView1.Items[6].SubItems.Add("Не существует");
             }
+            else
+            {
+                MessageBox.Show("Не все поля были заполнены");
+            }
+            
 
 
         }
